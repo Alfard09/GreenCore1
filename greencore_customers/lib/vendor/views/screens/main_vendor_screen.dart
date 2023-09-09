fa3d8dc8f1fa94ba2a75d6592123dde8edffa2f4
alfard09 <alfardkoshy2024@mca.ajce.in>
2023-09-10 00:37:09 +0530

    Vendor side- bottom navigation and pages added

--- greencore_customers/lib/vendor/views/screens/main_vendor_screen.dart
+++ greencore_customers/lib/vendor/views/screens/main_vendor_screen.dart
@@ -1,20 +1,68 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:greencore_1/vendor/views/screens/earning_screen.dart';
+import 'package:greencore_1/vendor/views/screens/edit_product_screen.dart';
+import 'package:greencore_1/vendor/views/screens/upload_screen.dart';
+import 'package:greencore_1/vendor/views/screens/vendor_logout_screen.dart';
+import 'package:greencore_1/vendor/views/screens/vendor_order_screen.dart';
 
-class MainVendorScreen extends StatelessWidget {
+class MainVendorScreen extends StatefulWidget {
+  @override
+  State<MainVendorScreen> createState() => _MainVendorScreenState();
+}
+
+class _MainVendorScreenState extends State<MainVendorScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  int _pageIndex = 0;
+
+  List<Widget> _pages = [
+    EarningScreen(),
+    UploadScreen(),
+    EditProductScreen(),
+    VendorOrderScreen(),
+    VendorLogoutScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () async {
-            await _auth.signOut();
-          },
-          child: Text("Signout"),
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        onTap: (value) {
+          setState(() {
+            _pageIndex = value;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+
+        unselectedItemColor: Colors.black,
+        // Color(0xff734b6d),
+        selectedItemColor: Color(0xff42275a),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.money_dollar),
+            label: 'EARNINGS',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.upload),
+            label: 'UPLOAD',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.edit),
+            label: 'EDIT',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'ORDERS',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout),
+            label: 'LOGOUT',
+          ),
+        ],
       ),
+      body: _pages[_pageIndex],
     );
   }
 }
