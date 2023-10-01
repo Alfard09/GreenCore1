52855c3cbe095e624c3d1c30dc4800a769956c1e
alfard09 <alfardkoshy2024@mca.ajce.in>
2023-10-02 00:53:39 +0530

    order screen in progress

--- greencore_customers/lib/vendor/views/screens/vendor_order_screen.dart
+++ greencore_customers/lib/vendor/views/screens/vendor_order_screen.dart
@@ -1,12 +1,173 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class VendorOrderScreen extends StatelessWidget {
-  const VendorOrderScreen({super.key});
+  String formattedDate(date) {
+    final outputDateFormat = DateFormat('dd/MM/yyyy');
+
+    final outputDate = outputDateFormat.format(date);
+
+    return outputDate;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Orders Screen'),
+    final Stream<QuerySnapshot> _ordersStream = FirebaseFirestore.instance
+        .collection('orders')
+        .where('vendorId', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff42275a),
+        elevation: 1,
+        title: Text(
+          'My Orders',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _ordersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Color(0xff42275a),
+              ),
+            );
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              return Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 14,
+                      child: document['accepted'] == true
+                          ? Icon(Icons.delivery_dining)
+                          : Icon(Icons.access_time),
+                    ),
+                    title: document['accepted'] == true
+                        ? Text(
+                            'Accepted',
+                            style: TextStyle(color: Colors.green.shade700),
+                          )
+                        : Text(
+                            'Not Accepted',
+                            style: TextStyle(color: Colors.red.shade700),
+                          ),
+                    trailing: Text(
+                      'Amount' +
+                          " " +
+                          document['productPrice'].toStringAsFixed(2),
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    subtitle: Text(
+                      formattedDate(
+                        document['orderDate'].toDate(),
+                      ),
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  ExpansionTile(
+                    title: Text(
+                      'Order Details',
+                      style: TextStyle(
+                        color: Color(0xff42275a),
+                        fontSize: 15,
+                      ),
+                    ),
+                    subtitle: Text('View Order Details'),
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Image.network(
+                            document['productImage'][0],
+                          ),
+                        ),
+                        title: Text(document['productName']),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(
+                                  'Quantity',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  document['quantity']
+                                      .toString(), //selectedQuantity
+                                ),
+                              ],
+                            ),
+                            document['accepted'] == true
+                                ? Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text('Schedule Delivery Date'),
+                                      Text(
+                                        formattedDate(
+                                          document['scheduleDate'].toDate(),
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                : Text(''),
+                            ListTile(
+                              title: Text(
+                                'Buyer Details',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                ),
+                              ),
+                              subtitle: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(document['fullName']),
+                                  Text(document['email']),
+                                  Text(document['address']),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              );
+            }).toList(),
+          );
+        },
+      ),
     );
   }
 }
