1962190df2d56f4df74465581c219a86cd7043f3
alfard09 <alfardkoshy2024@mca.ajce.in>
2024-02-22 10:38:31 +0530

    admin dashboard added, vendor report done

--- greencore1_admin_panel/lib/views/screens/side_bar_screen/dashboard_screen.dart
+++ greencore1_admin_panel/lib/views/screens/side_bar_screen/dashboard_screen.dart
@@ -1,7 +1,31 @@
+// import 'package:flutter/material.dart';
+
+// class DashboardScreen extends StatelessWidget {
+//   static const String routeName = '\DashboardScreen';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Container(
+//         alignment: Alignment.topLeft,
+//         padding: const EdgeInsets.all(10),
+//         child: Text(
+//           "Dashboard",
+//           style: TextStyle(
+//             fontWeight: FontWeight.w700,
+//             fontSize: 30,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
-  static const String routeName = '\DashboardScreen';
+  static const String routeName = '/dashboard';
 
   @override
   Widget build(BuildContext context) {
@@ -9,13 +33,103 @@ class DashboardScreen extends StatelessWidget {
       child: Container(
         alignment: Alignment.topLeft,
         padding: const EdgeInsets.all(10),
-        child: Text(
-          "Dashboard",
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 30,
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Dashboard",
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ), // Add some spacing between "Dashboard" and the boxes
+            FutureBuilder(
+              future: _getCounts(),
+              builder: (context, AsyncSnapshot<Map<String, int>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  final productsCount = snapshot.data!['products'];
+                  final sellersCount = snapshot.data!['sellers'];
+                  final usersCount = snapshot.data!['users'];
+
+                  return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          //crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            _buildBox("Products", productsCount.toString()),
+                            SizedBox(width: 20),
+                            _buildBox("Sellers", sellersCount.toString()),
+                            SizedBox(width: 20),
+                            _buildBox("Users", usersCount.toString()),
+                          ],
+                        ),
+                      ]);
+                }
+              },
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Future<Map<String, int>> _getCounts() async {
+    final productsQuery =
+        await FirebaseFirestore.instance.collection('products').get();
+    final sellersQuery =
+        await FirebaseFirestore.instance.collection('vendors').get();
+    final usersQuery =
+        await FirebaseFirestore.instance.collection('buyers').get();
+
+    final productsCount = productsQuery.docs.length;
+    final sellersCount = sellersQuery.docs.length;
+    final usersCount = usersQuery.docs.length;
+
+    return {
+      'products': productsCount,
+      'sellers': sellersCount,
+      'users': usersCount,
+    };
+  }
+
+  Widget _buildBox(String title, String count) {
+    return Container(
+      width: 220,
+      height: 220,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(207, 48, 165, 2),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            count,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+            ),
+          ),
+        ],
       ),
     );
   }
--- greencore_customers/lib/vendor/views/screens/vendor_inner_screen/order_report.dart
+++ greencore_customers/lib/vendor/views/screens/vendor_inner_screen/order_report.dart
@@ -1,15 +1,257 @@
+// import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class OrdersReportPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Orders Report'),
+//       ),
+//       body: StreamBuilder(
+//         stream: FirebaseFirestore.instance.collection('orders').snapshots(),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return Center(
+//               child: CircularProgressIndicator(),
+//             );
+//           }
+//           if (snapshot.hasError) {
+//             return Center(
+//               child: Text('Error: ${snapshot.error}'),
+//             );
+//           }
+//           final orders = snapshot.data?.docs;
+//           return ListView.builder(
+//             itemCount: orders?.length,
+//             itemBuilder: (context, index) {
+//               var order = orders?[index];
+//               return Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Card(
+//                   color: Color.fromARGB(255, 84, 37, 104),
+//                   elevation: 4,
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(10.0),
+//                   ),
+//                   child: ListTile(
+//                     contentPadding: EdgeInsets.all(16.0),
+//                     title: Text(
+//                       order?['productName'],
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 18.0,
+//                       ),
+//                     ),
+//                     subtitle: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         SizedBox(height: 8.0),
+//                         Text(
+//                           'Buyer Email: ${order?['email']}',
+//                           style: TextStyle(
+//                             fontSize: 14.0,
+//                             color: Colors.white,
+//                           ),
+//                         ),
+//                         SizedBox(height: 4.0),
+//                         Text(
+//                           'Quantity: ${order?['quantity']}',
+//                           style: TextStyle(
+//                             fontSize: 14.0,
+//                             color: Colors.white,
+//                           ),
+//                         ),
+//                         SizedBox(height: 4.0),
+//                         Text(
+//                           'Total Price: \$${order?['totalPrice']}',
+//                           style: TextStyle(
+//                             fontSize: 14.0,
+//                             color: Colors.white,
+//                           ),
+//                         ),
+//                         SizedBox(height: 4.0),
+//                         Text(
+//                           'Order Date: ${order?['orderDate'].toDate().toString()}',
+//                           style: TextStyle(
+//                             fontSize: 14.0,
+//                             color: Colors.white,
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                     leading: CircleAvatar(
+//                       backgroundImage: NetworkImage(order?['productImage'][0]),
+//                     ),
+//                     onTap: () {
+//                       // Add onTap functionality if needed
+//                     },
+//                   ),
+//                 ),
+//               );
+//             },
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+//2
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:pdf/pdf.dart';
+// import 'package:pdf/widgets.dart' as pw;
+// import 'package:path_provider/path_provider.dart';
+// import 'package:open_file/open_file.dart';
+
+// class OrdersReportPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Orders Report'),
+//       ),
+//       body: StreamBuilder(
+//         stream: FirebaseFirestore.instance.collection('orders').snapshots(),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return Center(
+//               child: CircularProgressIndicator(),
+//             );
+//           }
+//           if (snapshot.hasError) {
+//             return Center(
+//               child: Text('Error: ${snapshot.error}'),
+//             );
+//           }
+//           final orders = snapshot.data!.docs;
+//           return ListView.builder(
+//             itemCount: orders.length,
+//             itemBuilder: (context, index) {
+//               var order = orders[index];
+//               return Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Card(
+//                   elevation: 4,
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(10.0),
+//                   ),
+//                   child: ListTile(
+//                     contentPadding: EdgeInsets.all(16.0),
+//                     title: Text(
+//                       order['productName'],
+//                       style: TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 18.0,
+//                       ),
+//                     ),
+//                     subtitle: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         SizedBox(height: 8.0),
+//                         Text(
+//                           'Buyer Email: ${order['email']}',
+//                           style: TextStyle(fontSize: 14.0),
+//                         ),
+//                         SizedBox(height: 4.0),
+//                         Text(
+//                           'Quantity: ${order['quantity']}',
+//                           style: TextStyle(fontSize: 14.0),
+//                         ),
+//                         SizedBox(height: 4.0),
+//                         Text(
+//                           'Total Price: \$${order['totalPrice']}',
+//                           style: TextStyle(fontSize: 14.0),
+//                         ),
+//                         SizedBox(height: 4.0),
+//                         Text(
+//                           'Order Date: ${order['orderDate'].toDate().toString()}',
+//                           style: TextStyle(fontSize: 14.0),
+//                         ),
+//                       ],
+//                     ),
+//                     leading: CircleAvatar(
+//                       backgroundImage: NetworkImage(order['productImage'][0]),
+//                     ),
+//                     onTap: () {
+//                       // Add onTap functionality if needed
+//                     },
+//                   ),
+//                 ),
+//               );
+//             },
+//           );
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () => _saveAsPdf(context),
+//         child: Icon(Icons.save),
+//       ),
+//     );
+//   }
+
+//   Future<void> _saveAsPdf(BuildContext context) async {
+//     final pdf = pw.Document();
+
+//     pdf.addPage(
+//       pw.Page(
+//         build: (pw.Context context) {
+//           return pw.Center(
+//             child: pw.Text(
+//               'Orders Report',
+//               style: pw.TextStyle(fontSize: 20, fontWeight: pw.FontWeight.bold),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+
+//     final Directory? directory = await getExternalStorageDirectory();
+//     final String path = directory!.path;
+//     final File file = File('$path/orders_report.pdf');
+//     await file.writeAsBytes(await pdf.save());
+
+//     OpenFile.open(file.path);
+//   }
+// }
+
+//3
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
+import 'package:open_file/open_file.dart';
+import 'package:intl/intl.dart';
 
-class OrdersReportPage extends StatelessWidget {
+class OrdersReportPage extends StatefulWidget {
+  @override
+  _OrdersReportPageState createState() => _OrdersReportPageState();
+}
+
+class _OrdersReportPageState extends State<OrdersReportPage> {
+  late Stream<QuerySnapshot> _ordersStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _ordersStream = FirebaseFirestore.instance.collection('orders').snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Orders Report'),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('orders').snapshots(),
+      body: FutureBuilder(
+        future: _getOrdersData(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(
@@ -21,15 +263,19 @@ class OrdersReportPage extends StatelessWidget {
               child: Text('Error: ${snapshot.error}'),
             );
           }
-          final orders = snapshot.data?.docs;
+          final orders = snapshot.data as List<DocumentSnapshot>;
+          if (orders.isEmpty) {
+            return Center(
+              child: Text('No orders found.'),
+            );
+          }
           return ListView.builder(
-            itemCount: orders?.length,
+            itemCount: orders.length,
             itemBuilder: (context, index) {
-              var order = orders?[index];
+              var order = orders[index];
               return Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Card(
-                  color: Color.fromARGB(255, 84, 37, 104),
                   elevation: 4,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10.0),
@@ -37,9 +283,8 @@ class OrdersReportPage extends StatelessWidget {
                   child: ListTile(
                     contentPadding: EdgeInsets.all(16.0),
                     title: Text(
-                      order?['productName'],
+                      order['productName'],
                       style: TextStyle(
-                        color: Colors.white,
                         fontWeight: FontWeight.bold,
                         fontSize: 18.0,
                       ),
@@ -49,40 +294,32 @@ class OrdersReportPage extends StatelessWidget {
                       children: [
                         SizedBox(height: 8.0),
                         Text(
-                          'Buyer Email: ${order?['email']}',
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.white,
-                          ),
+                          'Buyer Email: ${order['email']}',
+                          style: TextStyle(fontSize: 14.0),
                         ),
                         SizedBox(height: 4.0),
                         Text(
-                          'Quantity: ${order?['quantity']}',
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.white,
-                          ),
+                          'Quantity: ${order['selectedQuantity']}',
+                          style: TextStyle(fontSize: 14.0),
                         ),
                         SizedBox(height: 4.0),
                         Text(
-                          'Total Price: \$${order?['totalPrice']}',
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.white,
-                          ),
+                          'Total Price: ₹ ${order['totalPrice']}',
+                          style: TextStyle(fontSize: 14.0),
                         ),
                         SizedBox(height: 4.0),
+                        // Text(
+                        //   'Order Date: ${order['orderDate'].toDate().toString()}',
+                        //   style: TextStyle(fontSize: 14.0),
+                        // ),
                         Text(
-                          'Order Date: ${order?['orderDate'].toDate().toString()}',
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.white,
-                          ),
+                          'Order Date: ${DateFormat('yyyy-MM-dd').format(order['orderDate'].toDate())}',
+                          style: TextStyle(fontSize: 14.0),
                         ),
                       ],
                     ),
                     leading: CircleAvatar(
-                      backgroundImage: NetworkImage(order?['productImage'][0]),
+                      backgroundImage: NetworkImage(order['productImage'][0]),
                     ),
                     onTap: () {
                       // Add onTap functionality if needed
@@ -94,6 +331,79 @@ class OrdersReportPage extends StatelessWidget {
           );
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _saveAsPdf(context),
+        child: Icon(Icons.save),
+      ),
     );
+  }
+
+  Future<List<DocumentSnapshot>> _getOrdersData() async {
+    final querySnapshot = await _ordersStream.first;
+    return querySnapshot.docs;
+  }
+
+  Future<void> _saveAsPdf(BuildContext context) async {
+    final pdf = pw.Document();
+
+    final orders = await _getOrdersData();
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Column(
+              children: [
+                pw.Text(
+                  'Orders Report',
+                  style: pw.TextStyle(
+                      fontSize: 20, fontWeight: pw.FontWeight.bold),
+                ),
+                pw.SizedBox(height: 20),
+                pw.TableHelper.fromTextArray(
+                  border: pw.TableBorder.all(width: 1, color: PdfColors.grey),
+                  cellAlignment: pw.Alignment.center,
+                  headerDecoration: pw.BoxDecoration(color: PdfColors.grey300),
+                  headerHeight: 25,
+                  cellHeight: 25,
+                  cellAlignments: {
+                    0: pw.Alignment.centerLeft,
+                    1: pw.Alignment.centerLeft,
+                    2: pw.Alignment.centerLeft,
+                    3: pw.Alignment.centerLeft,
+                    4: pw.Alignment.centerLeft,
+                  },
+                  headerStyle: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                  data: <List<String>>[
+                    <String>[
+                      'Product Name',
+                      'Buyer Email',
+                      'Quantity',
+                      'Total Price',
+                      'Order Date'
+                    ],
+                    for (var order in orders)
+                      <String>[
+                        order['productName'],
+                        order['email'],
+                        order['selectedQuantity'].toString(),
+                        "₹" + " " + '${order['totalPrice']}',
+                        order['orderDate'].toDate().toString(),
+                      ]
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+
+    final Directory? directory = await getExternalStorageDirectory();
+    final String path = directory!.path;
+    final File file = File('$path/orders_report.pdf');
+    await file.writeAsBytes(await pdf.save());
+
+    OpenFile.open(file.path);
   }
 }
